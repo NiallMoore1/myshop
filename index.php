b3f2d59e2c8329e58d28599caf861bef8034c8b0
niall moore <moore.nialls@gmail.com>
2019-11-28 22:01:17 +0000

    creating navbar

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
         <title> On Line Store </title>
 		<link rel="stylesheet" href="styles/style.css" media="all">
     </head>
+<!--main container-->
 			<body>
 				<div class="main_wrapper">
+<!--header-->
 					<div class="header_wrapper">
 						<img id="logo" src="images/image-11.jpeg"/>
 						<img id="banner" src="images/image-22.jpeg"/>
 					</div>
+<!--/header-->
+<!--navbar-->
 							<div class="menubar">
+							<ul id="menu">
+								<li><a href="#">Home</a></li>
+								<li><a href="#">All Products</a></li>
+								<li><a href="#">My Account</a></li>
+								<li><a href="#">Sign Up</a></li>
+								<li><a href="#">Shopping Cart</a></li>
+								<li><a href="#">Contact Us</a></li>
+							</ul>
 							</div>
-						
+<!--search box-->
+							<div>search
+							</div>
+<!--/search box-->
 						
 							<div class="content_wrapper">
 							<div id="sidebar"></div>
@@ -21,7 +36,8 @@
 						</div>
 					
 				</div>
-			
+<!--/navbar-->		
+<!--sidebar-->	
 				
 			
 			
