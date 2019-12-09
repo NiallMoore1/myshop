e68b7e606eae06da8f5a7fa68244489a38cf1543
niall moore <moore.nialls@gmail.com>
2019-12-09 20:32:12 +0000

    creating the checkout page

---
+++ customer_login.php
@@ -0,0 +1,36 @@
+
+
+<div>
+
+<form method="post" action="">
+
+    <table width ="500" align="center" bgcolor="skyblue">
+    
+        <tr align="center">
+            <td colspan="3"><h2>Login Or Register</h2></td>
+        </tr>
+        <tr>
+            <td align="right"><b>Email:</b></td>
+            <td><input type="text" name="email" placeholder="enter email"/></td>
+        </tr>
+    
+        <tr>
+            <td align="right"><b>Password:</b></td>
+            <td><input type="password" name="pass" placeholder="enter password"/></td>
+        </tr>
+        
+        <tr align="left">
+            <td colspan="3"><a href="checkout.php?forgot_pass">Forgot Password</a></td>
+        </tr>
+        <tr align="center">
+            <td colspan="3"><input type="submit" name="login" value="Login"/></td>
+            <td><h3 style="float:right;"><a href="customer_register.php"> Register Here</a></h3></td>
+        </tr>
+        
+    </table>
+        
+
+
+
+</form>
+</div>
