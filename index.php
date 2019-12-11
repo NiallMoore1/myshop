f9780fec1c4a26c6621a5108cf521d1e61957211
niall moore <moore.nialls@gmail.com>
2019-12-11 20:20:11 +0000

    Customer Login & Logout

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE>
 <?php
+session_start();
 include("functions/functions.php");
 ?>
 <html>
@@ -58,12 +59,34 @@ include("functions/functions.php");
 								<div id="shopping_cart">
 <!--Adding Shopping Cart-->			<span style="float:right; font-size:18px; padding:5px; line-height:40px;">
 										Welcome Guest! <b style="color:yellow;">Shopping Cart</b>Total Items:  <?php total_items();?>  Total Price: <?php total_price(); ?> <a href="cart.php" style ="color:yellow;">Go To Cart</a>
+										
+										
+<!--Customer login logout -->
+
+									<?php
+									if(!isset($_SESSION['customer_email'])){
+										
+										echo "<a href='checkout.php' style='color:orange;'>Login</a>";
+										
+									}
+									
+									else{
+										
+										echo "<a href='logout.php' style='color:orange;'>Logout</a>";
+									}
+									
+									
+									
+									?>
+<!--Customer login logout end-->
 									</span>
 						
 								</div>
 								
 								
 <!-- Getting IP address<?php echo $ip=getIp() ;?>-->
+
+
 
 
 									<div id="products_box">
