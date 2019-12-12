7ac42600a87b42c0aaad80bd61569cd6741a8252
niall moore <moore.nialls@gmail.com>
2019-12-12 14:02:01 +0000

    change password part 1

---
+++ customer/change_pass.php
@@ -0,0 +1,27 @@
+<h2 style="text-align:center;">Change Your Password</h2>
+<form action="" method="post"> 
+	
+	<table align="center" width="600">
+	<tr>
+	<td align="right"><b>Enter Current Password:</b></td>
+	<td><input type="password" name="current_pass" required></td>
+	</tr>
+	
+	<tr>
+	<td align="right"><b>Enter New Password:</b></td>
+	<td><input type="password" name="new_pass" required></td>
+	</tr>
+	
+	<tr>
+	<td align="right"><b>Enter New Password Again:</b></td>
+	<td><input type="password" name="new_pass_again" required></td>
+	</tr>
+	
+	<tr align="center">
+	<td colspan="3"><input type="submit" name="change_pass" value="Change Password"/></td>
+	</tr>
+	
+	</table>
+
+
+</form>
