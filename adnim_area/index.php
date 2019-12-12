4e648ace134a4a253ac3211f025742caed6934d2
niall moore <moore.nialls@gmail.com>
2019-12-12 16:05:09 +0000

    creating the admin panel

---
+++ adnim_area/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE>
+
+<html>
+
+	<head>
+	
+		<title>This is admin panel</title>
+		<link rel="stylesheet" href="styles/style.css" media="all" />
+	</head>
+	
+<body>
+
+	
+	<div class="main_wrapper">
+	
+	
+		<div id="header"></div>
+		<div id="right">Manage Content</div>
+		<div id="left"></div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</div>
+</body>
+</html>
