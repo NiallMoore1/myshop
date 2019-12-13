64ec80db1636733a980b74093720b2dce87b3690
niall moore <moore.nialls@gmail.com>
2019-12-13 14:29:16 +0000

    adding new index file

---
+++ adnim_area/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>This Is Admin Panel</title>
+		<link rel="stylesheet" href="styles/style.css" media="all"></link>
+	</head>
+	
+<body>
+
+	<div class="main_wrapper">
+	
+	
+		<div id="header"></div>
+		
+			<div id="right">Manage Content</div>
+			<div id="left"></div>
+	</div>
+
+</body>
+</html>
