cf265afb0d9c60e7a7ac6bacf7d4fcd237e054da
niall moore <moore.nialls@gmail.com>
2019-12-14 15:32:44 +0000

    adding links + inserting new product in admin

--- adnim_area/index.php
+++ adnim_area/index.php
@@ -12,8 +12,43 @@
 	
 		<div id="header"></div>
 		
-			<div id="right">Manage Content</div>
-			<div id="left"></div>
+			<div id="right">
+				<h2 style="text-align:center; color:gold;">Manage Content</h2>
+				
+					<a href="index.php?insert_product">Insert New Product</a>
+					<a href="index.php?view_products">View All Products</a>
+					<a href="index.php?insert_cat">Insert New Categories</a>
+					<a href="index.php?view_cats">View All Categories</a>
+					<a href="index.php?insert_brand">Insert New Brand</a>
+					<a href="index.php?view_brands">View All Brands</a>
+					<a href="index.php?view_customers">View Customers</a>
+					<a href="index.php?view_orders">View Orders</a>
+					<a href="index.php?view_payments">View Payments</a>
+					<a href="logout.php">Admin Logout</a>
+					
+			
+			
+			
+			
+			
+			</div>
+			
+			
+			
+			
+			
+			
+			
+			<div id="left">
+			<?php 
+		if(isset($_GET['insert_product'])){
+		
+		include("insert_product.php"); 
+		
+		}
+		
+		?>
+			</div>
 	</div>
 
 </body>
